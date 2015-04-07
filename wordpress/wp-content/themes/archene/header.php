e820008644ef28a16fef09f61d92632dc601c656
Ray McMahan <ray.mcmahan1@gmail.com>
2015-04-07 17:24:21 -0500

    Closes #11 Created logo image. Made it so you can change logo in WordPress Customizer. Made logo span full width of page.

--- wordpress/wp-content/themes/archene/header.php
+++ wordpress/wp-content/themes/archene/header.php
@@ -23,6 +23,14 @@
 <body>
 	<div class="container-fluid">
     	<header>
+        	<?php if ( get_header_image() ) : ?>
+                <div id="site-header">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+                        <!--img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"-->
+                    </a>
+                </div>
+            <?php endif; ?>
             <div class="page-header">                
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                 
