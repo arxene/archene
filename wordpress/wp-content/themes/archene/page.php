ea00227037c67d3d588fde238e972306854394d6
Ray McMahan <ray.mcmahan1@gmail.com>
2015-04-12 00:56:30 -0500

    Revert "Noticed that this called content-page.php, but I don't have that template part yet, so I changed it to call the basic content.php."
    This reverts commit 80f409aa74eee6ef943c98ea2c9c0843124e8e16.

--- wordpress/wp-content/themes/archene/page.php
+++ wordpress/wp-content/themes/archene/page.php
@@ -8,7 +8,7 @@
                         while ( have_posts() ) : the_post();
                     
                             // Include the page content template.
-                            get_template_part( 'content', 'none' );
+                            get_template_part( 'content', 'page' );
                     
                             // If comments are open or we have at least one comment, load up the comment template.
                             if ( comments_open() || get_comments_number() ) :
