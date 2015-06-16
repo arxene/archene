625e518808e156722cc989e6c279ef5ba07df126
Ray McMahan <ray.mcmahan1@gmail.com>
2015-06-16 15:14:28 -0500

    Closes #34. I think usage of the Bootstrap row and col classes without it being inside a container or container-fluid div caused the width to be greater than 100%. Removing those made the width 100% with no horizontal scrollbar and it looks ok on mobile still.

--- wordpress/wp-content/themes/archene/footer-menu.php
+++ wordpress/wp-content/themes/archene/footer-menu.php
@@ -1,7 +1,3 @@
 				<div class="footer-menu">
-					<div class="row">
-                    	<div class="col-xs-12">
-                        	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
-                        </div>
-                    </div>
-                </div><!-- #footer-menu -->
+          <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+        </div><!-- #footer-menu -->
